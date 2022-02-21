83901b4cca0ce7a9afe74971d29389faeedb966b
NadyaKuts <nadiakuz97@gmail.com>
2022-02-22 00:40:04 +0300

    fix api

--- api/stratz.js
+++ api/stratz.js
@@ -1,19 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const instance = () => axios.create({
-  baseURL: "https://api.opendota.com/api",
-  headers: {
-  },
-});
+const instance = axios.create({
+  baseURL: 'https://api.opendota.com/api',
+})
 
 export const dotaListApi = {
   getHeroStats() {
-    let heroStats = []
-    instance()
-    .get('/heroStats')
-    .then((response) => {
-      heroStats = response.data})
-    .catch((error) => error.response)
-    return heroStats
-  }
-} 
+    return instance
+      .get('/heroStats')
+      .then((response) => {
+        return response.data
+      })
+      .catch((error) => error.response)
+  },
+}
